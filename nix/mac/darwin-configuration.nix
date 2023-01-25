6971c29039d3a6220a6f63d5ff318f39b20acf0f
Gabe Saenz <29664619+gabesaenz@users.noreply.github.com>
2023-01-24 22:15:37 -0600

    Update Mac Nix config.

--- nix/mac/darwin-configuration.nix
+++ nix/mac/darwin-configuration.nix
@@ -4,17 +4,28 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-      bat
+      # doom-emacs dependencies
+      git
+      ripgrep
+      coreutils # optional
+      fd # optional
+      fontconfig # added because of doom doctor warning
+
+      emacsUnstable
   ];
 
   # Fonts
+  fonts.fontDir.enable = true;
   fonts.fonts = with pkgs; [
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
     # Devanagari
     annapurna-sil
-    # (nerdfonts.override { fonts = [ "FireCode" ]; })
+    # nerd fonts
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    # doom-emacs
+    emacs-all-the-icons-fonts
   ];
 
   # Use a custom configuration.nix location.
@@ -29,6 +40,25 @@
   programs.zsh.enable = true;  # default shell on catalina
   programs.fish.enable = true;
 
+  services.emacs.enable = true;
+  services.emacs.package = pkgs.emacsUnstable;
+
+  services.yabai.enable = true;
+  services.yabai.package = pkgs.yabai;
+  services.skhd.enable = true;
+
+  homebrew.enable = true;
+  homebrew.casks = [ "alacritty" ];
+  homebrew.onActivation.cleanup = "zap";
+  # homebrew.onActivation.upgrade = true;
+
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+    }))
+  ];
+
+
   # Home Manager
   imports = [ <home-manager/nix-darwin> ];
 
@@ -40,9 +70,12 @@
     home.stateVersion = "22.05";
     home.packages = with pkgs; [
       exa
+      bat
+      # adobe-reader # i686 Linux package (not supported) 2023-01-23
       # neovide
-      alacritty
-      rustup
+      # alacritty
+      # firefox # not supported on darwin 2023-01-22
+      # rustup
       # rustdesk # not supported on darwin 2022-12-02
       # pomotroid # not supported on darwin 2022-12-12
     ];
