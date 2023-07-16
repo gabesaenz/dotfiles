f3c7de2b574bc6dc13aa1d040df26601601a9162
gabe <29664619+gabesaenz@users.noreply.github.com>
2023-07-16 20:20:28 +0200

    Update saved custom settings.

--- .config/.doom.d/custom.el
+++ .config/.doom.d/custom.el
@@ -5,7 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(doc-view-continuous t)
- '(safe-local-variable-values '((projectile-project-compilation-cmd . "./build.sh"))))
+ '(safe-local-variable-values '((projectile-project-compilation-cmd . "./build.sh")))
+ '(smtpmail-smtp-server "mail.gmx.net")
+ '(smtpmail-smtp-service 25))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
