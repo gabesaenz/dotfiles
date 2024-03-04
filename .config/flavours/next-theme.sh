3285bafec4b0c07a8cbbf9fad1516a4359680966
Gabe Saenz <29664619+gabesaenz@users.noreply.github.com>
2024-03-04 11:25:24 +0100

    Update theme switching scripts.

--- .config/flavours/next-theme.sh
+++ .config/flavours/next-theme.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-themes="theme-list.txt"
+dir=$(readlink -f "$0" | xargs dirname)
+themes="${dir}/theme-list.txt"
 current=$(flavours current)
-line=$(sed -n "/${current}/=" ${themes})
-next=$(head --lines=1 ${themes})
+line=$(sed -n "/${current}/=" "${themes}")
+next=$(head --lines=1 "${themes}")
 if [ -n "$line" ]; then
     line=$((line + 1))
-    line=$(sed -n "${line}p" ${themes})
+    line=$(sed -n "${line}p" "${themes}")
 fi
 if [ -n "$line" ]; then
     next=${line}
