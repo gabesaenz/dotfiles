38223c4c729c43d38be8eaa3be29a708e02b77e3
Gabe Saenz <29664619+gabesaenz@users.noreply.github.com>
2025-05-11 13:20:02 +0200

    Use fish as vterm shell in emacs.

--- doomdir/config.el
+++ doomdir/config.el
@@ -200,8 +200,8 @@
 
 ;; shell related fixes suggested by doom doctor
 (setq shell-file-name (executable-find "bash"))
-(setq-default vterm-shell (executable-find "nu"))
-(setq-default explicit-shell-file-name (executable-find "nu"))
+(setq-default vterm-shell (executable-find "fish"))
+(setq-default explicit-shell-file-name (executable-find "fish"))
 
 ;; use builtin eww browser for online lookup (dictionary)
 (setq +lookup-open-url-fn #'eww)
