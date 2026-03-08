86b9f901b7f2a01425d8ae1ac5e084b990e7decf
gabe <29664619+gabesaenz@users.noreply.github.com>
2026-03-08 21:02:45 +0100

    Add emacs shell config back in.

--- nixos-laptop/home/emacs/doomdir/config.el
+++ nixos-laptop/home/emacs/doomdir/config.el
@@ -246,9 +246,9 @@
 	message-send-mail-function #'message-send-mail-with-sendmail))
 
 ;; shell related fixes suggested by doom doctor
-;; (setq shell-file-name (executable-find "bash"))
-;; (setq-default vterm-shell (executable-find "zsh"))
-;; (setq-default explicit-shell-file-name (executable-find "zsh"))
+(setq shell-file-name (executable-find "bash"))
+(setq-default vterm-shell (executable-find "zsh"))
+(setq-default explicit-shell-file-name (executable-find "zsh"))
 
 ;; use builtin eww browser for online lookup (dictionary)
 (setq +lookup-open-url-fn #'eww)
