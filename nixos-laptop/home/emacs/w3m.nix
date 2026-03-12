ad6a90c23f971be2075e4154a61bfa8c5de70060
gabe <29664619+gabesaenz@users.noreply.github.com>
2026-03-12 21:46:45 +0100

    Update w3m config.

--- nixos-laptop/home/emacs/w3m.nix
+++ nixos-laptop/home/emacs/w3m.nix
@@ -7,29 +7,57 @@
     packages = builtins.readFile ./w3m-packages.el;
     config = builtins.readFile ./w3m-config.el;
   };
-  ### add a desktop entry
-  # [Desktop Entry]
-  # Name=emacs-w3m
-  # GenericName=Emacs Web Browser
-  # X-GNOME-FullName=Emacs w3m Web Browser
-
-  # Encoding=UTF-8
-  # Version=1.0
-  # Comment=Using emacsclient
-  # NoDisplay=true
-
-  # Exec=emacsclient --eval "(browse-url (replace-regexp-in-string \"'\" \"\" \"%u\"))"
-
-  # Terminal=false
-  # X-MultipleArgs=false
-  # Type=Application
-  # Icon=firefox-esr
-  # Categories=Network;WebBrowser;
-  # StartupWMClass=Firefox-esr
-  # StartupNotify=true
-  # MimeType=x-scheme-handler/unknown;x-scheme-handler/about;text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
-
-  ### set as default browser
-  # xdg-settings set default-web-browser emacs-w3m.desktop
-
+  # doesn't do much unless it's also the default browser (which it isn't currently) so commenting this out
+  # xdg.desktopEntries = {
+  #   "emacs-w3m" = {
+  #     name = "emacs-w3m";
+  #     genericName = "Emacs Web Browser";
+  #     comment = "Using emacsclient";
+  #     noDisplay = true;
+  #     exec = ''emacsclient --eval "(browse-url (replace-regexp-in-string \"'\" \"\" \"%u\"))"'';
+  #     terminal = false;
+  #     settings = {
+  #       X-GNOME-FullName = "Emacs w3m Web Browser";
+  #       Encoding = "UTF-8";
+  #       Version = "1.0";
+  #       X-MultipleArgs = "false";
+  #       StartupWMClass = "Firefox-esr";
+  #     };
+  #     type = "Application";
+  #     icon = "firefox-esr";
+  #     categories = [
+  #       "Network"
+  #       "WebBrowser"
+  #     ];
+  #     startupNotify = true;
+  #     mimeType = [
+  #       "x-scheme-handler/unknown"
+  #       "x-scheme-handler/about"
+  #       "text/html"
+  #       "text/xml"
+  #       "application/xhtml+xml"
+  #       "application/xml"
+  #       "application/vnd.mozilla.xul+xml"
+  #       "application/rss+xml"
+  #       "application/rdf+xml"
+  #       "image/gif"
+  #       "image/jpeg"
+  #       "image/png"
+  #       "x-scheme-handler/http"
+  #       "x-scheme-handler/https"
+  #     ];
+  #   };
+  # };
+  # set as default browser
+  # don't do this since it removes all other existing defaultApplication settings
+  # xdg.mimeApps = {
+  #   enable = true;
+  #   defaultApplications = {
+  #     "text/html" = "emacs-w3m.desktop";
+  #     "x-scheme-handler/http" = "emacs-w3m.desktop";
+  #     "x-scheme-handler/https" = "emacs-w3m.desktop";
+  #     "x-scheme-handler/about" = "emacs-w3m.desktop";
+  #     "x-scheme-handler/unknown" = "emacs-w3m.desktop";
+  #   };
+  # };
 }
