8f04a4a22d7578891e3ed0c6f23ef243ce060220
gabe <29664619+gabesaenz@users.noreply.github.com>
2026-03-12 17:34:10 +0100

    Remove geoclue since it isn't working.

--- nixos-laptop/configuration.nix
+++ nixos-laptop/configuration.nix
@@ -258,15 +258,15 @@
     ];
   };
 
-  # Set Geoclue as the default location provider
-  location.provider = "geoclue2";
+  # # Set Geoclue as the default location provider
+  # location.provider = "geoclue2";
 
-  # Enable Geoclue
-  services.geoclue2 = {
-    enable = true;
-    # if using beaconDB
-    geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
-  };
+  # # Enable Geoclue
+  # services.geoclue2 = {
+  #   enable = true;
+  #   # if using beaconDB
+  #   geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+  # };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
