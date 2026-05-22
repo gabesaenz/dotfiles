b0d33765fb592558f330e495df0c7128f0a682c9
gabe <29664619+gabesaenz@users.noreply.github.com>
2026-05-22 19:05:58 +0200

    Up font size to help with Greek.

--- nixos-laptop/home/emacs/doom/config.el
+++ nixos-laptop/home/emacs/doom/config.el
@@ -25,9 +25,9 @@
 ;;       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 
 ;; (setq doom-font (font-spec :family "VictorMono Nerd Font" :size 18))
-(setq doom-font (font-spec :family "monospace" :size 18))
+(setq doom-font (font-spec :family "monospace" :size 20))
 ;; (setq doom-variable-pitch-font (font-spec :family "sansSerif" :size 18))
-(setq doom-variable-pitch-font (font-spec :family "Noto Sans" :size 18))
+(setq doom-variable-pitch-font (font-spec :family "Noto Sans" :size 20))
 ;; WARNING: if you specify a size for the emoji font it will hard-lock any usage of this font to that size. It's rarely a good idea to do so!
 (setq doom-emoji-font (font-spec :family "emoji"))
 ;; don't set doom-symbol-font / main fallback unicode font
