3c78c0a91383182def91ef39c19ace6359328c23
gabe <29664619+gabesaenz@users.noreply.github.com>
2026-08-08 17:40:18 +0200

    Update niri config locations for noctalia theming.

--- nixos-laptop/home/dms.nix
+++ nixos-laptop/home/dms.nix
@@ -30,4 +30,13 @@
   doom-config = {
     config = "(setq doom-theme 'dank-emacs)";
   };
+
+  xdg.configFile."niri/config.kdl".text = ''
+    // import DankMaterialShell settings
+    include "dms.kdl"
+  '';
+  xdg.configFile.noctalia-niri-config = {
+    source = ./niri/dms.kdl;
+    target = "niri/dms.kdl";
+  };
 }
--- nixos-laptop/home/niri.nix
+++ nixos-laptop/home/niri.nix
@@ -5,10 +5,10 @@
   ...
 }:
 {
-  # xdg.configFile."niri/config.kdl".text = builtins.readFile ./niri/config.kdl;
-  xdg.configFile.niri-config = {
-    source = ./niri;
-    target = "niri";
-    recursive = true;
-  };
+  xdg.configFile."niri/config.kdl".text = builtins.readFile ./niri/config.kdl;
+  # xdg.configFile.niri-config = {
+  #   source = ./niri;
+  #   target = "niri";
+  #   recursive = true;
+  # };
 }
